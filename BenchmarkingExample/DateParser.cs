674387bb4a13cbb3c0464fafae777d3886529b54
EsamMagdy <dev.esam2014@gmail.com>
2023-09-13 14:42:41 +0300

    add benchmarking project

---
+++ BenchmarkingExample/DateParser.cs
@@ -0,0 +1,33 @@
+﻿namespace BenchmarkingExample
+{
+    public class DateParser
+    {
+        public int GetYearFromDateTime(string dateTimeAsString)
+        {
+            var datTime = DateTime.Parse(dateTimeAsString);
+
+            return datTime.Year;
+        }
+        public int GetYearFromSplit(string dateTimeAsString)
+        {
+            var splitOnHypen = dateTimeAsString.Split('-');
+
+            return int.Parse(splitOnHypen[0]);
+
+        } 
+        public int GetYearFromSubstring(string dateTimeAsString)
+        {
+            var indexOfHypen = dateTimeAsString.IndexOf('-');
+
+            return int.Parse(dateTimeAsString.Substring(0, indexOfHypen));
+
+        }
+        public int GetYearFromSpan(ReadOnlySpan<char> dateTimeAsSpan)
+        {
+            var indexOfHypen = dateTimeAsSpan.IndexOf('-');
+
+            return int.Parse(dateTimeAsSpan.Slice(0, indexOfHypen));
+
+        }
+    }
+}
---
+++ BenchmarkingExample/DateParserBenchMarkers.cs
@@ -0,0 +1,47 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace BenchmarkingExample
+{
+    // A diagnoser can attach to your benchmark and get some useful info.
+    [MemoryDiagnoser]
+
+    // Orderers allows customizing the order of benchmark results in the summary table.
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+
+    /* used to display the rank of benchmarked methods 
+     * or jobs in the summary table generated after running benchmarks. 
+     * It helps you quickly identify which method 
+     * or job performed the best in terms of a specific metric (e.g., execution time or memory allocation).
+    */
+    [RankColumn]
+    public class DateParserBenchMarkers
+    {
+        private const string DateTime = "2023-09-13T11:51:06Z";
+        private readonly DateParser Parser = new DateParser();
+
+        [Benchmark]
+        public void GetYearFromDateTime()
+        {
+            Parser.GetYearFromDateTime(DateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSplit()
+        {
+            Parser.GetYearFromSplit(DateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSubstring()
+        {
+            Parser.GetYearFromSubstring(DateTime);
+        }
+
+        [Benchmark]
+        public void GetYearFromSpan()
+        {
+            Parser.GetYearFromSpan(DateTime);
+        }
+    }
+}

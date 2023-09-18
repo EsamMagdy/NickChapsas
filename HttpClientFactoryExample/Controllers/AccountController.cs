1dce4898110e38713da0c2660f988331c5ea88fb
encooders-esammagdy <dev.esam2014@gmail.com>
2023-09-18 09:58:32 +0300

    add httpclientfactory

---
+++ HttpClientFactoryExample/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientFactoryExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+      
+
+        [HttpGet("LoadAccount")]
+        public async Task<ActionResult> LoadAccount()
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.github.com/"),
+                DefaultRequestHeaders =
+                {
+                    {"Accept","application/vnd.github.v3+json" },
+                    {"User-Agent","HttpClientFactoryExample" }
+                }
+            };
+
+            var accountInfo = await httpClient.GetStringAsync("users/EsamMagdy");
+
+            return Ok(accountInfo);
+        }
+    }
+}
